7c9033c6f8a37a21762f898eafb18bc25648f213
Kanjiz1 <kanjis007@gmail.com>
2018-10-10 15:18:52 -0500

    Added Tests

--- pixel-city/pixel-city/Utilities/Constants.swift
+++ pixel-city/pixel-city/Utilities/Constants.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-let API_KEY = ""
+let API_KEY = "977181eb0f8b9bd79a3b085bd8244832"
 
 func flickrURL(forApiKey key: String, withAnnotation annotation: DroppablePin, andNumberOfPhotos number: Int) -> String{
     return "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=\(API_KEY)&lat=\(annotation.coordinate.latitude)&lon=\(annotation.coordinate.longitude)&radius=2&radius_units=km&per_page=\(number)&format=json&nojsoncallback=1"
